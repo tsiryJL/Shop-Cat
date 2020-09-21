eaa8d06f41a0ec051e688ac97628a12981616383
tsiryjl <tsiry.razanakoto@gmail.com>
2020-09-21 20:14:21 +0200

    change production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,13 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "projet-boutique-e-commerce_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "appne_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
